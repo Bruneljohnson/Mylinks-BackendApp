3857e01e1b517df8c578ad9efe657b62c9ff3883
Bruneljohnson <photography@bruneljohnson.com>
2022-04-30 15:14:29 +0200

    final edits

--- app.js
+++ app.js
@@ -36,9 +36,8 @@ const corsOptions = {
   origin: (origin, callback) => {
     if (!origin) return callback(null, true);
     if (whitelist.indexOf(origin) === -1) {
-      var msg =
-        'The CORS policy for this site does not ' +
-        'allow access from the specified Origin.';
+      const msg =
+        'The CORS policy for this site does not allow access from the specified Origin.';
       return callback(new Error(msg), false);
     }
     return callback(null, true);
